675d1a114f3cb5ec5e4b5e9c24ddbaecc384570b
Ling <cathyxie@brandeis.edu>
2021-09-11 13:23:11 -0400

    attempt to add database

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn --chdir project server:app
 heroku ps:scale web=1
+release: python manage.py db upgrade
--- project/server/config.py
+++ project/server/config.py
@@ -16,7 +16,7 @@ class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = 'postgres://fyjsblkjrxaymu:ee4bebc2600d195e9fd9e06b85b5d19df6ec327483d10c50facff831fcdde120@ec2-44-198-100-81.compute-1.amazonaws.com:5432/d2voth52ml1119'
+    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
 
 
 
